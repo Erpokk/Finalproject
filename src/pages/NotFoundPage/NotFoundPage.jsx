96a3d0bd648dd4c15636e256b496e762ff14eec8
Maksym Adamenko <maimav56@gmail.com>
2025-01-19 23:41:57 +0200

    Add Not Found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,32 @@
-import DocumentTitle from '../../components/DocumentTitle';
-import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button.jsx";
+import Container from "../../components/Container/Container.jsx";
 
-export default function RegisterPage() {
+import DocumentTitle from "../../components/DocumentTitle.jsx";
+import scss from "./HomePage.module.scss";
+
+export default function HomePage() {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <DocumentTitle>Registration</DocumentTitle>
-      <RegisterForm />
-    </div>
+    <>
+      <DocumentTitle>Not Found Page</DocumentTitle>
+      <section>
+        <Container isPadding={false}>
+          <div className={scss.hero}>
+            <div className={scss.innerWrapper}>
+              <h1 className="light">Page Not Found</h1>
+              <h2 className="light">Please go back yp home page</h2>
+            </div>
+            <Button onClick={handleNavigate} option={"redBtn"}>
+              Go Back Home
+            </Button>
+          </div>
+        </Container>
+      </section>
+    </>
   );
 }
