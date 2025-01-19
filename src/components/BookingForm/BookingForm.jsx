57e9c73d4c9ea6d2ee1480751776cb3820294b81
Maksym Adamenko <maimav56@gmail.com>
2025-01-20 00:37:25 +0200

    Add TOASTER

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -2,6 +2,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup"; // Для валидации
 import scss from "./BookingForm.module.scss";
 import Button from "../Button/Button";
+import toast, { Toaster } from "react-hot-toast";
 
 const BookingForm = () => {
   // Валидационная схема
@@ -23,77 +24,82 @@ const BookingForm = () => {
 
   const handleSubmit = (values, { resetForm }) => {
     console.log("Form Submitted:", values);
+    toast.success("Booking submitted successfully!"); // Успешное сообщение
     resetForm();
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {() => (
-        <Form>
-          {/* Поле Name */}
-          <div className={scss.inputWrapper}>
-            <Field
-              className={scss.input}
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name*"
-            />
-            <ErrorMessage name="name" component="div" />
-          </div>
+    <>
+      <Toaster position="top-right" reverseOrder={false} />{" "}
+      {/* Инициализация Toaster */}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <Form>
+            {/* Поле Name */}
+            <div className={scss.inputWrapper}>
+              <Field
+                className={scss.input}
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name*"
+              />
+              <ErrorMessage name="name" component="div" />
+            </div>
 
-          {/* Поле Email */}
-          <div className={scss.inputWrapper}>
-            <Field
-              className={scss.input}
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email*"
-            />
-            <ErrorMessage name="email" component="div" />
-          </div>
+            {/* Поле Email */}
+            <div className={scss.inputWrapper}>
+              <Field
+                className={scss.input}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email*"
+              />
+              <ErrorMessage name="email" component="div" />
+            </div>
 
-          {/* Поле Booking Date */}
-          <div className={scss.inputWrapper}>
-            <Field
-              className={scss.input}
-              type="date"
-              id="bookingDate"
-              name="bookingDate"
-              placeholder="Booking date*"
-            />
-            <ErrorMessage
-              name="bookingDate"
-              component="div"
-              placeholder="Email*"
-            />
-          </div>
+            {/* Поле Booking Date */}
+            <div className={scss.inputWrapper}>
+              <Field
+                className={scss.input}
+                type="date"
+                id="bookingDate"
+                name="bookingDate"
+                placeholder="Booking date*"
+              />
+              <ErrorMessage
+                name="bookingDate"
+                component="div"
+                placeholder="Email*"
+              />
+            </div>
 
-          {/* Поле Comment */}
-          <div>
-            <Field
-              className={scss.textArea}
-              as="textarea"
-              id="comment"
-              name="comment"
-              placeholder="Comment*"
-              rows="5"
-            />
-            <ErrorMessage name="comment" component="div" />
-          </div>
+            {/* Поле Comment */}
+            <div>
+              <Field
+                className={scss.textArea}
+                as="textarea"
+                id="comment"
+                name="comment"
+                placeholder="Comment*"
+                rows="5"
+              />
+              <ErrorMessage name="comment" component="div" />
+            </div>
 
-          {/* Кнопка Submit */}
-          <Button type="submit" option={"redBtn"}>
-            Send
-          </Button>
-        </Form>
-      )}
-    </Formik>
+            {/* Кнопка Submit */}
+            <Button type="submit" option={"redBtn"}>
+              Send
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
